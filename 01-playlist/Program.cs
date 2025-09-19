3fca25686f8fc63045df47da9fafe112d13651e6
Johan Janérs <johanjaners@gmail.com>
2025-09-19 18:08:15 +0200

    01-playlist: added class song w property, constructor

---
+++ 01-playlist/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Song
+{
+    public string Title { get; set; } // property
+    public Song(string title) // constructor
+    {
+        Title = title;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Song s = new Song("Nothing Else Matters"); // create object
+        Console.WriteLine(s.Title); // use property
+    }
+}
