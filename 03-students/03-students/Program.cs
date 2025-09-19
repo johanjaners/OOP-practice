f0d056c3f356152bccf052ef92b5decb2de7b49a
Johan Janérs <johanjaners@gmail.com>
2025-09-19 20:24:33 +0200

    03-students: added student class, classroom

--- 03-students/03-students/Program.cs
+++ 03-students/03-students/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+class Student
+{
+    public string Name { get; set; } // property of student with name
+
+    public int Grade { get; set; } // property
+    public int Year { get; set; } // property
+
+    public Student(string name, int grade, int year)
+    {
+        this.Name = name;
+        this.Grade = grade;
+        this.Year = year;
+    }
+}
+
+class Classroom
+{
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        
+    }
+}
