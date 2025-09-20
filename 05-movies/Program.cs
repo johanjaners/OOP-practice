2be60e74fe949fd3e7c13a0478d64af22619f672
Johan Janérs <johanjaners@gmail.com>
2025-09-20 15:34:25 +0200

    05-movies:add  new console app, Class Movie property Title/ year, get set, constructor  + class Collection, Movies list

---
+++ 05-movies/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+class Movie
+{
+    public string Title { get; set; }
+
+    public Movie(string title)
+    {
+        this.Title = title;
+    }
+}
+
+
+class Collection
+{
+    public List<Movie> Movies { get; } = new(); // property , intializes empty at start, readable only
+
+    public void AddMovie(Movie m)
+    {
+        Movies.Add(m);
+    }
+
+
+
+}
