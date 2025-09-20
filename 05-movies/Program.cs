117fb2e61a4180b50319f06e21f33db63afbe011
Johan Janérs <johanjaners@gmail.com>
2025-09-20 15:39:40 +0200

    05-movies: class Program, new COlleciton C, add movies, print title / year

--- 05-movies/Program.cs
+++ 05-movies/Program.cs
@@ -1,17 +1,13 @@
-﻿
-
-
-class Movie
+﻿class Movie
 {
     public string Title { get; set; }
-
-    public Movie(string title)
+    public int Year { get; set; }
+    public Movie(string title, int year)
     {
         this.Title = title;
+        this.Year = year;
     }
 }
-
-
 class Collection
 {
     public List<Movie> Movies { get; } = new(); // property , intializes empty at start, readable only
@@ -20,7 +16,18 @@ class Collection
     {
         Movies.Add(m);
     }
+}
+class Program
+{
+    static void Main()
+    {
+        Collection C = new Collection();
 
+        C.AddMovie(new Movie("Xmen", 2010));
+        C.AddMovie(new Movie("Thor", 2012));
 
+        Console.WriteLine(C.Movies[0].Title);
 
+        Console.WriteLine(C.Movies[1].Year);
+    }
 }
