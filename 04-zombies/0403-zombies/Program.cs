f1ce943f47c8ebd92cc3fab3574971dbca5ebc06
Johan Janérs <johanjaners@gmail.com>
2025-09-21 13:58:22 +0200

    0403-zombies: new console, new class Room, properties, constr. methods

---
+++ 04-zombies/0403-zombies/Program.cs
@@ -0,0 +1,19 @@
+﻿class Room
+{
+    public int C { get; set; }
+    public string Z { get; set; }
+    public Room(int c) => this.C = c;
+    public List<string> Zs { get; } = new();
+    public bool IsFull() => Zs.Count > C;
+    public void AddZ(string z)
+    {
+        if (C == 0) return;
+        if (IsFull() && C > 0)
+        {
+            Zs.Add(z);
+            Zs.RemoveAt(0);
+            return;
+        }
+        Zs.Add(z);
+    }
+}
