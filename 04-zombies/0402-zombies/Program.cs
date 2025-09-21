55be576d180fcccf7b6ac7e46aab455056cabc87
Johan Janérs <johanjaners@gmail.com>
2025-09-21 13:04:09 +0200

    new 0402-zombies console app, Class Room added, properties; Capacity, Zombie, Zombies List, constructor for capacity, methods; IsFull(), AddZombie() with 0 limit and FIFO eviciton

---
+++ 04-zombies/0402-zombies/Program.cs
@@ -0,0 +1,27 @@
+﻿class Room
+{
+    public int Capacity { get; set; }
+    public string Zombie { get; set; }
+    public List<Zombie> Zombies { get; } = new();
+    public Room(int capacity) => this.Capacity = capacity;
+
+    public bool IsFull() => Zombies.Count >= Capacity;
+
+    public void AddZombie(string zombie)
+    {
+        if (Capacity == 0) return;
+        if (IsFull() && Capacity > 0)
+        {
+            Zombies.Add(zombie);
+            Zombies.RemoveAt(0);
+            return;
+        }
+        Zombies.Add(zombie);
+    }
+
+
+
+
+
+
+}
