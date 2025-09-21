30b6e1f0d0d734896dbeb165efee3305e8c4a8b1
Johan Janérs <johanjaners@gmail.com>
2025-09-21 07:55:29 +0200

    0401-zombies: Class Room:  properties, constructor, method ,, Class Program, Main

--- 04-zombies/0401-zombies/Program.cs
+++ 04-zombies/0401-zombies/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Room
+{
+    public int Capacity { get; set; } // property of room , readabke setable
+    public string Zombie { get; set; } // 
+    public List<Zombie> Zombies { get; set; } = new();
+    public Room(int capacity) => this.Capacity = capacity;
+    public AddZombie(string zombie) => Zombies.Add(zombie);
+    public boolean isFull() => Zombies.Count >= Capacity;
+
+}
+
+
+
+
+class Program
+{
+    static void Main()
+    {
+        var R = new Room(0);
+
+        Console.WriteLine(R.isFull);
+    }
+}
