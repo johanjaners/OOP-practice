341f51bfcc54b47061ec80fbe700148f61f0713c
Johan Janérs <johanjaners@gmail.com>
2025-09-21 14:00:09 +0200

    0403-z : class program, main, lines

--- 04-zombies/0403-zombies/Program.cs
+++ 04-zombies/0403-zombies/Program.cs
@@ -17,3 +17,30 @@
         Zs.Add(z);
     }
 }
+
+
+class Program
+{
+    static void Main()
+    {
+        var r0 = new Room(1);
+        Console.WriteLine("empty room not full: " + !r0.IsFull());
+
+        var r1 = new Room(0);
+        r1.AddZombie("A");
+        Console.WriteLine("cap 0 cannot add: " + (r1.Zombies.Count == 0 && r1.IsFull()));
+
+        var r2 = new Room(1);
+        r2.AddZombie("A");
+        Console.WriteLine("one-roomer full after one: " + r2.IsFull());
+
+        var r3 = new Room(2);
+        r3.AddZombie("A");
+        Console.WriteLine("two-roomer not full after one: " + !r3.IsFull());
+
+        var r4 = new Room(1);
+        r4.AddZombie("A");
+        r4.AddZombie("B"); // FIFO
+        Console.WriteLine("fifo keeps newest: " + (r4.Zombies.Count == 1 && r4.Zombies[0] == "B"));
+    }
+}
