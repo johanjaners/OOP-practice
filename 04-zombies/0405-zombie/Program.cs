4afa0d437d93dfe3799ca31de414b066b292d436
Johan Janérs <johanjaners@gmail.com>
2025-09-21 21:11:02 +0200

    0405-zombie: empy room not full + zero room fit non

--- 04-zombies/0405-zombie/Program.cs
+++ 04-zombies/0405-zombie/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//room with no capacity cannot fit any zombies
+//one-roomer becomes full when a zombie is added
+//two-roomer is not full when a zombie is added
+//second zombie consumes first zombie when added to a one-roomer
+
+class Room
+{
+    public int C { get; set; }
+    public Room(int c) => this.C = c;
+    public string? Z { get; set; }
+    public List<string> Zs { get; set; } = new();
+    public bool IsFull() => Zs.Count >= C;
+    public void AddZ(string z)
+    {
+        if (C == 0) return;
+        Zs.Add(z);
+    }
+
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        //empty room that fits one zombie is not full
+        var r0 = new Room(1);
+        Console.WriteLine("empty one room not full: " + !r0.IsFull());
+
+        //room with no capacity cannot fit any zombies
+        var r1 = new Room(0);
+        r1.AddZ("A");
+        Console.WriteLine("no cap fit no zombie: " + (r1.Zs.Count == 0 && r1.IsFull()));
+    }
+}
