253ff47af27f2faca6d731906f1553d4a4050127
Johan Janérs <johanjaners@gmail.com>
2025-09-22 13:07:19 +0200

    0406-zombies: empty one room not full

--- 04-zombies/0406-zombies/Program.cs
+++ 04-zombies/0406-zombies/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Room
+{
+    public int C { get; set; }
+    public Room(int c) => this.C = c;
+    public List<string> Zs { get; set; } = new();
+    public bool IsFull() => Zs.Count >= C;
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        //empty room that fits one zombie is not full
+        var r0 = new Room(1);
+        Console.WriteLine("empty one room not full: " + !r0.IsFull());
+        //room with no capacity cannot fit any zombies
+        //one-roomer becomes full when a zombie is added
+        //two-roomer is not full when a zombie is added
+        //second zombie consumes first zombie when added to a one-roomer    
+
+    }
+}
