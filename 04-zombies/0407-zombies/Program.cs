6d3dc204d9d087f9b8507a49a002e51c4f758ea1
Johan Janérs <johanjaners@gmail.com>
2025-09-22 14:24:05 +0200

    0407-zombies: new Console, Class Zombie + Room

---
+++ 04-zombies/0407-zombies/Program.cs
@@ -0,0 +1,16 @@
+﻿class Zombie
+{
+    public string Name { get; set}    
+    public Zombie(string name) => this.Name = name;
+}
+class Room
+{
+    public int C { get; set; }
+    public Room(int c) => this.C = c;
+}
+        
+        //empty room that fits one zombie is not full
+        //room with no capacity cannot fit any zombies
+        //one-roomer becomes full when a zombie is added
+        //two-roomer is not full when a zombie is added
+        //second zombie consumes first zombie when added to a one-roomer
