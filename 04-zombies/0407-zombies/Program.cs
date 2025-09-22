66022c46bc5de7d25cd9c725a2191bd66a9fd8bb
Johan Janérs <johanjaners@gmail.com>
2025-09-22 14:41:56 +0200

    0407-zombies: zero room dont fit any

--- 04-zombies/0407-zombies/Program.cs
+++ 04-zombies/0407-zombies/Program.cs
@@ -8,8 +8,12 @@ class R
     public int C { get; set; }
     public R(int c) => this.C = c;
     public List<Z> Zs { get; set; } = new();
-
     public bool IsFull() => Zs.Count >= C;
+    public void AddZ(Z z)
+    {
+        if (C == 0) return;
+        Zs.Add(z);
+    }
 }
 class Program
 {
@@ -19,9 +23,11 @@ class Program
         var r0 = new R(1);
         Console.WriteLine("Empty one room not full: " + !r0.IsFull());
         //room with no capacity cannot fit any zombies
+        var r1 = new R(0);
+        r1.AddZ(new Z("A"));
+        Console.WriteLine("0 room dont fit any: " + (r1.Zs.Count == 0));
         //one-roomer becomes full when a zombie is added
         //two-roomer is not full when a zombie is added
         //second zombie consumes first zombie when added to a one-roomer
     }
-}    
-        
+}
--- 04-zombies/0407-zombies/obj/Debug/net9.0/0407-zombies.AssemblyInfo.cs
+++ 04-zombies/0407-zombies/obj/Debug/net9.0/0407-zombies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0407-zombies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3dc204d9d087f9b8507a49a002e51c4f758ea1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d9ad98a190d649dbad4dfba89024cda3b95c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("0407-zombies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0407-zombies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
