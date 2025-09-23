3a2ed3ff5ead82da6ec1f1045ca787b06a7bdb50
Johan Janérs <johanjaners@gmail.com>
2025-09-23 18:28:41 +0200

    0411-zombies: no cap room dont fit any

--- 04-zombies/0411-zombies/Program.cs
+++ 04-zombies/0411-zombies/Program.cs
@@ -4,23 +4,25 @@
     public R(int c) => this.C = c;
     public List<string> Zs { get; set; } = new();
     public bool IsFull() => Zs.Count >= C;
-
-
+    public void AddZ(string z)
+    {
+        if (C == 0) return;
+        Zs.Add(z);
+    }
 }
-
-
-
 class Program
 {
     static void Main()
     {
         //empty room that fits one zombie is not full
         var r0 = new R(1);
-        Console.WriteLine("empty one room not full" + !r0.IsFull());
+        Console.WriteLine("empty one room not full: " + !r0.IsFull());
         //room with no capacity cannot fit any zombies
+        var r1 = new R(0);
+        r1.AddZ("A");
+        Console.WriteLine("no cap room dont fit any: " + (r1.Zs.Count == 0));
         //one-roomer becomes full when a zombie is added
         //two-roomer is not full when a zombie is added
         //second zombie consumes first zombie when added to a one-roomer   
     }
-}
-	
+}	
