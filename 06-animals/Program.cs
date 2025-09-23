067e7d27d87797ca3f9c5b1692ad3ee464bcf240
Johan Janérs <johanjaners@gmail.com>
2025-09-23 12:37:36 +0200

    06-animals: base class: animal, dog inherits

---
+++ 06-animals/Program.cs
@@ -0,0 +1,20 @@
+﻿//base class
+class Animal
+{
+    public void Eat() => Console.WriteLine("Eating..");
+}
+//derived class, inherits Animal, (if non, inherits object which is ultimate base class)
+class Dog : Animal
+{
+    public void Bark() => Console.WriteLine("Woof!");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog d = new Dog();
+        d.Eat(); // inherited from animal
+        d.Bark(); // defined in dog
+    }
+}
