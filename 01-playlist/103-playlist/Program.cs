fee5cd79ff4f897d571442f01f6f1ca713104b5c
Johan Janérs <johanjaners@gmail.com>
2025-09-24 12:53:57 +0200

    103-playlist: class Song, class Playlist, methods AddSong, RemoveSong, FindSong

---
+++ 01-playlist/103-playlist/Program.cs
@@ -0,0 +1,30 @@
+﻿class Song
+{
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public Song(string title, string artist)
+    {
+        this.Title = title;
+        this.Artist = artist;
+    }
+}
+class Playlist
+{
+    public List<Song> Songs { get; } = new();
+    public void AddSong(Song s) => Songs.Add(s);
+    public void AddSong(string title, string artist) => Songs.Add(new Song(title, artist));
+    public void RemoveSong(Song s) => Songs.Remove(s);
+    public void RemoveSong(string title, string artist)
+    {
+        var songToRemove = Songs.find(r => r.Title == title && r.Artist == artist);
+        if (songToRemove != null) Songs.Remove(songToRemove);
+    }
+    public Song? FindSong(string title, string artist) => Songs.Find(x => x.Title == title && x.Artist == artist);
+}
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
