e501d16cf52d5897f86f9607767c800ef472621c
Johan Janérs <johanjaners@gmail.com>
2025-09-24 13:56:12 +0200

    105-playlist: class Song, title, artist

---
+++ 01-playlist/105-playlist/Program.cs
@@ -0,0 +1,10 @@
+﻿class Song
+{
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public Song(string title, string artist)
+    {
+        this.Title = title;
+        this.Artist = artist;
+    }
+}
