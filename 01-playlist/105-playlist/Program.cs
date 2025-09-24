faa54600f0ce0a854e92e513c612615c7253f221
Johan Janérs <johanjaners@gmail.com>
2025-09-24 13:59:41 +0200

    105-playlist: class Playlist, add, remove, find

--- 01-playlist/105-playlist/Program.cs
+++ 01-playlist/105-playlist/Program.cs
@@ -8,3 +8,10 @@
         this.Artist = artist;
     }
 }
+class Playlist
+{
+    public List<Song> Songs { get; } = new();
+    public void AddSong(Song s) => Songs.Add(s);
+    public void RemoveSong(Song s) => Songs.Remove(s);
+    public Song? FindSong(string title, string artist) => Songs.Find(s => s.Title == title && s.Artist == artist);
+}
