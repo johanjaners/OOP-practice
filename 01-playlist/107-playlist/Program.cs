6fab4a548667d6c2f7615e3c0775f9a9bf78d624
Johan Janérs <johanjaners@gmail.com>
2025-09-24 14:45:24 +0200

    107-playlist: class Song, Playlist

---
+++ 01-playlist/107-playlist/Program.cs
@@ -0,0 +1,17 @@
+﻿class Song
+{
+    public string T { get; set; }
+    public string A { get; set; }
+    public Song(string t, string a)
+    {
+        this.T = t;
+        this.A = a;
+    }
+}
+class Playlist
+{
+    public List<Song> Songs { get; } = new();
+    public void AddS(Song s) => Songs.Add(s);
+    public void RemoveS(Song s) => Songs.Remove(s);
+    public Song? FindS(string t, string a) => Songs.Find(s => s.T == t && s.A == a);
+}
