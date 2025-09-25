c56b979dbc4a12ad6a4e21840b4dbaa55e8054d4
Johan Janérs <johanjaners@gmail.com>
2025-09-25 09:23:13 +0200

    0701-calculator: static methods Add/Subtract

---
+++ 07-calculator/0701-calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿class Calculator
+{
+    public static int Add(int a, int b) => a + b;
+    public static int Subtract(int a, int b) => a - b;
+}
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Add: " + Calculator.Add(5, 3)); // 8
+        Console.WriteLine("Subtract: " + Calculator.Subtract(5, 3)); // 2
+    }
+}
